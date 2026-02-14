29932488a626bab4ee932b8b2a9b7a05d8a75a1f
Akash <dwivediakash1702@gmail.com>
2026-02-14 18:02:56 +0530

    change-6

---
+++ start.js
@@ -0,0 +1,6 @@
+/**
+ * Bootstrap entry so "npm start" always runs from repo root.
+ * Run: node start.js (from directory where package.json lives).
+ * This loads src/index.js; all imports then resolve correctly (no src/src).
+ */
+import "./src/index.js";
